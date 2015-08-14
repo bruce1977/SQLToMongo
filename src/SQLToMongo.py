9cab7f7117cb8b969d9cb3df0d557e6a755a6809
Gavin Gong <418513370@qq.com>
2015-08-14 09:55:06 +0800

    8.13

--- src/SQLToMongo.py
+++ src/SQLToMongo.py
@@ -16,77 +16,119 @@ def GetArgv():
 
 
 
-#清除Mongo数据库
-def MongoClea(mgDB,mgCollection,conn):
-    db = conn[mgDB]
-    print(conn.database_names())
-    collection = db[mgCollection]
-    collection.remove()
-
-'''
-def MongoiInsert(mgPath='mgPath',mgPort='mgPort',mgDB='mgDB',mgCollection='mgCollection'):
-    conn = pymongo.MongoClient(mgPath,mgPort)
-    MongoClear(mgDB,mgCollection,conn)
-    con = pymssql.connect(server='10.10.73.208:4399', user='sa', password='123456', database="PSPMS_Dev")
-    cursor = con.cursor()
-    cursor.execute('select * from company')
-    for row in cursor:
-        MongoCompanyInsert(row[1],row[2],row[0])
-        print(row)
-    con.close()
-'''
-class SQLtoMongo:
+class SQLServer:
     sqlPath =''
     sqlUser = ''
     sqlPWD = ''
     sqlDB = ''
     con = None
     #构造函数创建于SQL连接
-    def __init__(self,sqlServer,sqlUser,sqlPWD,sqlDB):
-        self.sqlPath = sqlServer
-        self.sqlUser = sqlUser
-        self.sqlPWD = sqlPWD
-        self.sqlDB = sqlDB
+    def __init__(self,host,port,username,pwd,database,provider):
+        self.sqlPath = host+':'+str(port)
+        self.sqlUser = username
+        self.sqlPWD = pwd
+        self.sqlDB = database
         self.con = pymssql.connect(server=self.sqlPath, user=self.sqlUser, password=self.sqlPWD, database=self.sqlDB)
         print('SQLConnected Successfully')
 
-    #向MG数据库转换
-    def StartTransPort(self,mgPath,mgPort,mgDB,mgCollection):
-        conn = pymongo.MongoClient(mgPath,mgPort)
-        MongoClea(mgDB,mgCollection,conn)
-        cursor = self.con.cursor()
+class MongoServer:
+    host = ''
+    port = ''
+    database = ''
+    username = ''
+    pwd = ''
+    conn = None
+    def __init__(self,host,port,username,pwd,database):
+        self.host = host
+        self.port = str(port)
+        self.database = database
+        self.username = username
+        self.pwd = pwd
+        self.conn = pymongo.MongoClient('mongodb://'+ username +':'+ pwd +'@' +host+':'+str(port))
+        print(self.conn.server_info())
+        print('Mongo Connected')
+
+    def TransFromSQL(self,collection,sqlcon,table,mgcollection):
+        mgPath = self.host
+        mgPort = str(self.port)
+        mgDB = self.database
+        mgUser = self.username
+        mgPWD = self.pwd
+        sqlTable = table
+        mgCollection = mgcollection
+        self.MongoClear(mgDB,mgCollection,self.conn)
+        cursor = sqlcon.cursor()
         column = {}
-        rowDone = {}
         i1 = 0
-        cursor.execute('select name from syscolumns where id=(select max(id) from sysobjects where xtype=\'u\' and name= \''+mgCollection+'\')')
+        #cursor.execute('select name from syscolumns where id=(select max(id) from sysobjects where xtype=\'u\' and name= \''+mgCollection+'\')')
+        cursor.execute('select name from syscolumns where id=object_id(N\''+sqlTable+'\')')
+        db = self.conn[mgDB]
+        collect = db[mgCollection]
         for r in cursor:
-            column[i1] = str(r).strip(',').strip(')').strip('(')
+            column[i1] = str(r).strip(',').strip(')').strip('(').strip(',').strip('\'')#去除数组分割剩下的垃圾
             i1 = i1+1
-        cursor.execute('select * from '+mgCollection)
+        collect.create_index(r[0])
+        cursor.execute('select * from '+sqlTable)
         for row in cursor:
             i = 0
             rowDone = {}
             for things in row:
                 rowDone[column[i]] = things
                 i = i+1
-            self.MongoInsert(rowDone,conn,mgDB,mgCollection)
-            print(row)
+            try:
+                self.MongoInsert(rowDone,self.conn,mgDB,mgCollection)
+            except:
+                print('Insert Failed')
+            print(rowDone)
 
-    #向Mongo插入数据
-    def MongoInsert(self,row,conn,mgDB,mgCollection):
-        db = conn[mgDB]
-        print(conn.database_names())
-        collection = db[mgCollection]
+    def TransFromSQL2(self,collection,sqlcon,table,mgcollection,SQLcolumn,MGcolumn):
+        mgPath = self.host
+        mgPort = str(self.port)
+        mgDB = self.database
+        mgUser = self.username
+        mgPWD = self.pwd
+        sqlTable = table
+        mgCollection = mgcollection
+        column = MGcolumn
+        self.MongoClear(mgDB,mgCollection,self.conn)
+        cursor = sqlcon.cursor()
+        SQLcolumn = {}
+        i1 = 0
+        db = self.conn[mgDB]
+        collect = db[mgCollection]
+        collect.create_index(r[0])
+        cursor.execute('select' 'from '+sqlTable)
+        for row in cursor:
+            i = 0
+            rowDone = {}
+            for things in row:
+                rowDone[column[i]] = things
+                i = i+1
+            try:
+                self.MongoInsert(rowDone,self.conn,mgDB,mgCollection)
+            except:
+                print('Insert Failed')
+            print(rowDone)
+
+
+    def MongoInsert(self,row,conn,database,collection):
+        db = conn[database]
+        #print(conn.database_names())
+        collection = db[collection]
         account = collection
         account.insert(row)
 
+    def MongoClear(self,mgDB,mgCollection,conn):
+        db = conn[mgDB]
+        print(conn.database_names())
+        collection = db[mgCollection]
+        collection.remove()
 
-
-fin = open('Config.json','r')
+fin = open('Config2.json','r')
 ConfigString = fin.read()
 decodejson = json.loads(ConfigString)
-StM = SQLtoMongo(**decodejson[0])
-for row in decodejson:
-    if(row != decodejson[0]):
-        StM.StartTransPort(**row)
-        print(row)
+sql = SQLServer(**decodejson['server']['source'])
+mg = MongoServer(**decodejson['server']['target'])
+s = decodejson['tables'][0]['source']
+mg.TransFromSQL('Company',sql.con,decodejson['tables'][0]['source'],decodejson['tables'][0]['target'])
+
